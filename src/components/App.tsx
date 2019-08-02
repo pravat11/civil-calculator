d67ef8615a1c62180443e38690edb34bc16f5f9c
Prabhat Gautam <prabhatgautam@lftechnology.com>
2019-08-02 21:57:27 +0545

    master Cleanup and directory structuring

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const App: React.FC = () => {
+  return <div className="App" />;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './public';
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
