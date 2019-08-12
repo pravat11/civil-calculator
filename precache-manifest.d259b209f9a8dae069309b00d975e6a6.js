091615b347f815d321899c609907bd99b0abc350
Prabhat Gautam <prabhatgautam@lftechnology.com>
2019-08-12 22:19:13 +0545

    Updates

--- precache-manifest.d259b209f9a8dae069309b00d975e6a6.js
+++ precache-manifest.d259b209f9a8dae069309b00d975e6a6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c26301a945d4c9879e4646a29af0298",
+    "revision": "d39991e8e133f7a0929802cc7d0e3919",
     "url": "/civil-calculator/index.html"
   },
   {
-    "revision": "80c45cb1963a09791948",
+    "revision": "93f924246420c0017888",
     "url": "/civil-calculator/static/css/main.eb289ca7.chunk.css"
   },
   {
-    "revision": "128d2e4cfc7a5b29625a",
-    "url": "/civil-calculator/static/js/2.27d772f6.chunk.js"
+    "revision": "f682b8797fea6ac7b40e",
+    "url": "/civil-calculator/static/js/2.d96f7c90.chunk.js"
   },
   {
-    "revision": "80c45cb1963a09791948",
-    "url": "/civil-calculator/static/js/main.52cad8a1.chunk.js"
+    "revision": "93f924246420c0017888",
+    "url": "/civil-calculator/static/js/main.d7d7dad7.chunk.js"
   },
   {
     "revision": "a533d5a17ee9a95a3b11",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/civil-calculator/precache-manifest.d6ff49354c2b6a5c796929563735df91.js"
+  "/civil-calculator/precache-manifest.d259b209f9a8dae069309b00d975e6a6.js"
 );
 
 self.addEventListener('message', (event) => {
